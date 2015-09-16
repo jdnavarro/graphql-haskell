ee0e0c3d1f1c259555d246df1be1c9e5b9be0e35
Danny Navarro <j@dannynavarro.net>
2015-09-16 10:12:36 +0200

    Use Cabal data-files to access golden tests data
    Fixes #1.

--- tests/golden.hs
+++ tests/golden.hs
@@ -8,14 +8,14 @@ import qualified Data.Text.IO as TIO
 import Test.Tasty (defaultMain)
 import Test.Tasty.Golden (goldenVsString)
 
+import Paths_graphql (getDataFileName)
 import Data.GraphQL.Parser (document)
 
 main :: IO ()
 main = defaultMain
-     $ goldenVsString "kitchen-sink.graphql"
-                      "./tests/data/kitchen-sink.graphql.golden"
-                      (parse "./tests/data/kitchen-sink.graphql")
+   =<< goldenVsString "kitchen-sink.graphql"
+   <$> getDataFileName "tests/data/kitchen-sink.graphql.graphql.golden"
+   <*> (parse <$> getDataFileName "tests/data/kitchen-sink.graphql")
   where
     parse = fmap (parseOnly document) . TIO.readFile
         >=> pure . either B8.pack (flip B8.snoc '\n' . B8.pack . show)
-
