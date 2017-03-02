6a10e28ba85d593184f6a1885bbab2347466ea0e
Danny Navarro <j@dannynavarro.net>
2017-03-01 21:05:49 -0300

    Garden

--- Data/GraphQL/Execute.hs
+++ Data/GraphQL/Execute.hs
@@ -36,36 +36,3 @@ operation schema (AST.Core.Query flds) =
   Aeson.Object . HashMap.singleton "data"
              <$> Schema.resolve (NE.toList schema) (NE.toList flds)
 operation _ _ = error "Mutations not supported yet"
-
--- | Takes a variable substitution function and a @GraphQL@ document.
---   If the document contains one query (and no other definitions)
---   it applies the substitution to the query's set of selections
---   and then returns their fields.
--- rootFields :: Schema.Subs -> Document -> [Field]
--- rootFields subs (Document [DefinitionOperation (Query (Node _varDefs _ _ sels))]) =
---     Schema.fields $ substitute subs <$> sels
--- rootFields _ _ = []
-
--- | Takes a variable substitution function and a selection. If the
---   selection is a field it applies the substitution to the field's
---   arguments using 'subsArg', and recursively applies the substitution to
---   the arguments of fields nested in the primary field.
--- substitute :: Schema.Subs -> Selection -> Selection
--- substitute subs (SelectionField (Field alias name args directives sels)) =
---     SelectionField $ Field
---       alias
---       name
---       -- TODO: Get rid of `catMaybes`, invalid arguments should raise an error
---       (catMaybes $ subsArg subs <$> args)
---       directives
---       (substitute subs <$> sels)
--- substitute _ sel = sel
-
--- TODO: Support different value types
--- | Takes a variable substitution function and an argument. If the
---   argument's value is a variable the substitution is applied to the
---   variable's name.
--- subsArg :: Schema.Subs -> Argument -> Maybe Argument
--- subsArg subs (Argument n (ValueVariable (Variable v))) =
---     Argument n . ValueString <$> subs v
--- subsArg _ arg = Just arg
